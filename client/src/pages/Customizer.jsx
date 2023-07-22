c852192eaefb9ea7782ed64a9972fd8a6ead74a0
Sutiksh Verma <verma.su@northeastern.edu>
2023-07-22 02:29:46 -0700

    Update Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -53,7 +53,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://ai-tshirt-print.onrender.com', {
+      const response = await fetch('https://ai-tshirt-print.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
